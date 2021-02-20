c540a2aa868b92f75c2479ee9c5eb53213f554c2
AmandaHolliday <amandamholliday@gmail.com>
2021-02-19 19:51:59 -0500

    fixed font

--- portfolio/src/Components/About.js
+++ portfolio/src/Components/About.js
@@ -9,13 +9,13 @@ function About() {
             <br />
             <img src="https://see.fontimg.com/api/renderfont4/pALD/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjM0EzODM4IiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/QWJvdXQgTWU/wonderbar.png"/>
             <div className="aboutme">
-                <h2>Hi! I'm Amanda.</h2>
+                <h2><b>Hi! I'm Amanda.</b></h2>
                 <p className="paragraph">
                     I'm a creative software engineer & full stack web developer. My passion for art and originality leads me to be constantly eager to build from my unique ideas. 
                     <br />
                        With an extensive work background in customer service roles, management, and hospitality - my ability to communicate with clients (as well as my team) makes me a valuable, reliable, morale booster in any role.
                 </p>
-                <h3>Languages:</h3>
+                <h3><b>Languages:</b></h3>
                     <div className="languages">
                         <li>HTML </li>
                         <li>CSS </li>
--- portfolio/src/Components/Contact.js
+++ portfolio/src/Components/Contact.js
@@ -10,8 +10,8 @@ function contact() {
             <img src="https://see.fontimg.com/api/renderfont4/pALD/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjM0EzODM4IiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Q29udGFjdCBNZQ/wonderbar.png"/>
             <h2>Email me:</h2>
             <h3>amandamholliday@gmail.com</h3>
-            <i class="fab fa-linkedin"><a  target="_blank" href="https://www.linkedin.com/in/amanda-holliday-b3b07760/">linkedin</a></i> <br />
-            <i class="fab fa-github"><a target="_blank" href="https://github.com/amandamholliday">github</a></i>
+            <a target="_blank" href="https://www.linkedin.com/in/amanda-holliday-b3b07760/">linkedin</a><br />
+            <a target="_blank" href="https://github.com/amandamholliday">github</a>
         </div>
     )
 }
